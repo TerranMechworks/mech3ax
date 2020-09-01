4ae4ae36a0e56159638a7358a3a0952cb167e7f2
Toby Fleming <tobywf@users.noreply.github.com>
2020-08-31 17:43:27 -0700

    Add license subcommand/output

--- src/bin/rezbd/main.rs
+++ src/bin/rezbd/main.rs
@@ -32,6 +32,7 @@ struct JsonOpts {
 
 #[derive(Clap)]
 enum SubCommand {
+    License,
     Sound(ZipOpts),
     Interp(JsonOpts),
     Reader(ZipOpts),
@@ -208,5 +209,27 @@ fn main() -> Result<()> {
         SubCommand::Textures(opts) => textures(opts),
         SubCommand::Motion(opts) => motion(opts),
         SubCommand::Mechlib(opts) => mechlib(opts),
+        SubCommand::License => license(),
     }
+}
+
+fn license() -> Result<()> {
+    print!(r#"
+mech3rs extracts assets from the MechWarrior 3 game.
+Copyright (C) 2015-2020  Toby Fleming
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    "#);
+    Ok(())
 }
