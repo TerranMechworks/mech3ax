19f48033213e58553d9bf1db8ee1508a86760ad8
Toby Fleming <tobywf@users.noreply.github.com>
2020-10-13 18:10:47 -0700

    Add easier modding support to textures

--- src/bin/rezbd/main.rs
+++ src/bin/rezbd/main.rs
@@ -1,5 +1,6 @@
 mod commands;
 mod errors;
+mod modding;
 
 use clap::Clap;
 use errors::Result;
@@ -36,6 +37,19 @@ struct JsonOpts {
 }
 
 #[derive(Clap)]
+struct ModOpts {
+    #[clap(about = "The source ZIP path")]
+    input: String,
+    #[clap(about = "The destination ZBD path (will be overwritten)")]
+    output: String,
+    #[clap(
+        about = "When specified, load 'manifest.json' and PNG files instead of a ZIP",
+        long
+    )]
+    modding: bool,
+}
+
+#[derive(Clap)]
 enum SubCommand {
     #[clap(about = "Prints license information")]
     License,
@@ -48,7 +62,7 @@ enum SubCommand {
     #[clap(
         about = "Reconstruct 'rimage.zbd', 'rmechtex*.zbd', 'rtexture*.zbd', 'texture*.zbd' archives from ZIP"
     )]
-    Textures(ZipOpts),
+    Textures(ModOpts),
     #[clap(about = "Reconstruct 'motion.zbd' archives from ZIP")]
     Motion(ZipOpts),
     #[clap(about = "Reconstruct 'mechlib.zbd' archives from ZIP")]
@@ -66,7 +80,16 @@ fn main() -> Result<()> {
         SubCommand::Sounds(opts) => commands::sounds(opts),
         SubCommand::Interp(opts) => commands::interp(opts),
         SubCommand::Reader(opts) => commands::reader(opts),
-        SubCommand::Textures(opts) => commands::textures(opts),
+        SubCommand::Textures(ModOpts {
+            input,
+            output,
+            modding: false,
+        }) => commands::textures(ZipOpts { input, output }),
+        SubCommand::Textures(ModOpts {
+            input,
+            output,
+            modding: true,
+        }) => modding::textures(JsonOpts { input, output }),
         SubCommand::Motion(opts) => commands::motion(opts),
         SubCommand::Mechlib(opts) => commands::mechlib(opts),
         SubCommand::Gamez(opts) => commands::gamez(opts),
---
+++ src/bin/rezbd/modding.rs
@@ -0,0 +1,42 @@
+use image::{DynamicImage, GenericImageView, ImageFormat};
+use mech3rs::textures::{write_textures, TextureInfo};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Error, ErrorKind, Read};
+use std::path::Path;
+
+use crate::errors::Result;
+use crate::JsonOpts;
+
+pub(crate) fn textures(opts: JsonOpts) -> Result<()> {
+    let path = Path::new(&opts.input);
+    let mut input = BufReader::new(File::open(path)?);
+    let mut buf = Vec::new();
+    input.read_to_end(&mut buf)?;
+    let mut texture_infos: Vec<TextureInfo> = serde_json::from_slice(&buf)?;
+    let parent = path.parent().expect("Manifest path must have a parent");
+
+    let mut images: HashMap<String, DynamicImage> = texture_infos
+        .iter_mut()
+        .map(|info| {
+            let mut path = parent.to_path_buf();
+            path.push(info.name.clone());
+            path.set_extension("png");
+            let mut reader = image::io::Reader::new(BufReader::new(File::open(path)?));
+            reader.set_format(ImageFormat::Png);
+            let image = reader.decode()?;
+            let (width, height) = image.dimensions();
+            info.width = width as u16;
+            info.height = height as u16;
+            Ok((info.name.clone(), image))
+        })
+        .collect::<Result<_>>()?;
+
+    let mut output = BufWriter::new(File::create(opts.output)?);
+
+    write_textures(&mut output, &texture_infos, |name| {
+        images
+            .remove(name)
+            .ok_or_else(|| Error::new(ErrorKind::NotFound, name.to_string()).into())
+    })
+}
