4e4fff15f525625680e2c3c451c6814ffa081867
Toby Fleming <sourcecode@tobywf.com>
2021-12-12 17:53:03 -0800

    Replace vergen with time-rs and custom build script
    The version is now less informative (only the build timestamp), but it builds
    so much faster, with far fewer dependencies.

--- crates/mech3ax-version/build.rs
+++ crates/mech3ax-version/build.rs
@@ -1,19 +1,15 @@
-use vergen::{vergen, Config, ShaKind, TimeZone, TimestampKind};
+use time::OffsetDateTime;
 
 fn main() {
-    let mut config = Config::default();
-    let build = config.build_mut();
-    *build.enabled_mut() = true;
-    *build.timestamp_mut() = true;
-    *build.timezone_mut() = TimeZone::Utc;
-    *build.kind_mut() = TimestampKind::DateOnly;
-    *build.semver_mut() = false;
-    let git = config.git_mut();
-    *git.enabled_mut() = true;
-    *git.branch_mut() = false;
-    *git.commit_timestamp_mut() = false;
-    *git.semver_mut() = false;
-    *git.sha_mut() = true;
-    *git.sha_kind_mut() = ShaKind::Short;
-    vergen(config).unwrap();
+    let now = OffsetDateTime::now_utc();
+    println!(
+        "cargo:rustc-env={}={:04}-{:02}-{:02}T{:02}:{:02}:{:02}Z",
+        "MECH3AX_BUILD_DATE",
+        now.year(),
+        now.month() as u8,
+        now.day(),
+        now.hour(),
+        now.minute(),
+        now.second()
+    );
 }
--- crates/mech3ax-version/src/lib.rs
+++ crates/mech3ax-version/src/lib.rs
@@ -1,6 +1,1 @@
-pub const VERSION: &str = concat!(
-    env!("VERGEN_BUILD_DATE"),
-    " (",
-    env!("VERGEN_GIT_SHA_SHORT"),
-    ")"
-);
+pub const VERSION: &str = env!("MECH3AX_BUILD_DATE");
