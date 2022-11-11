19dbde5a22de8f96ce9d1b952fb993f79567d0bc
Toby Fleming <sourcecode@tobywf.com>
2022-11-11 10:06:39 -0800

    Dedupe meshes info code

--- crates/mech3ax-gamez/src/gamez/common.rs
+++ crates/mech3ax-gamez/src/gamez/common.rs
@@ -1,6 +1,62 @@
+use log::{debug, trace};
+use mech3ax_api_types::{static_assert_size, ReprSize as _};
+use mech3ax_common::io_ext::{CountingReader, CountingWriter};
+use mech3ax_common::{assert_that, Result};
+use std::io::{Read, Write};
+
 pub const SIGNATURE: u32 = 0x02971222;
 
 pub const VERSION_RC: u32 = 15;
 pub const VERSION_MW: u32 = 27;
 pub const VERSION_PM: u32 = 41;
 pub const VERSION_CS: u32 = 42;
+
+#[derive(Debug)]
+#[repr(C)]
+struct MeshesInfoC {
+    array_size: i32, // 00
+    count: i32,      // 04
+    index_max: i32,  // 08
+}
+static_assert_size!(MeshesInfoC, 12);
+pub const MESHES_INFO_C_SIZE: u32 = MeshesInfoC::SIZE;
+
+pub fn read_meshes_info_sequential(read: &mut CountingReader<impl Read>) -> Result<(i32, i32)> {
+    debug!(
+        "Reading mesh info ({}) at {}",
+        MeshesInfoC::SIZE,
+        read.offset
+    );
+    let info: MeshesInfoC = read.read_struct()?;
+    trace!("{:#?}", info);
+
+    assert_that!("mesh array size", info.array_size > 0, read.prev + 0)?;
+    assert_that!("mesh count", info.count < info.array_size, read.prev + 4)?;
+    assert_that!(
+        "mesh index max",
+        info.index_max == info.count,
+        read.prev + 8
+    )?;
+
+    Ok((info.array_size, info.count))
+}
+
+pub fn write_meshes_info_sequential(
+    write: &mut CountingWriter<impl Write>,
+    array_size: i32,
+    count: i32,
+) -> Result<()> {
+    debug!(
+        "Writing mesh info (rc, {}) at {}",
+        MeshesInfoC::SIZE,
+        write.offset
+    );
+    let info = MeshesInfoC {
+        array_size,
+        count,
+        index_max: count,
+    };
+    trace!("{:#?}", info);
+    write.write_struct(&info)?;
+    Ok(())
+}
--- crates/mech3ax-gamez/src/gamez/cs/meshes.rs
+++ crates/mech3ax-gamez/src/gamez/cs/meshes.rs
@@ -1,43 +1,23 @@
+use crate::gamez::common::{
+    read_meshes_info_sequential, write_meshes_info_sequential, MESHES_INFO_C_SIZE,
+};
 use crate::mesh::ng::{
     read_mesh_data, read_mesh_info, read_mesh_infos_zero, size_mesh, write_mesh_data,
     write_mesh_info, write_mesh_infos_zero, MESH_C_SIZE,
 };
-use log::{debug, trace};
-use mech3ax_api_types::{static_assert_size, MeshNg, ReprSize as _};
+use mech3ax_api_types::MeshNg;
 use mech3ax_common::io_ext::{CountingReader, CountingWriter};
 use mech3ax_common::{assert_len, assert_that, Result};
 use std::io::{Read, Write};
 
-#[derive(Debug)]
-#[repr(C)]
-struct MeshesInfoC {
-    array_size: i32, // 00
-    count: i32,      // 04
-    index_max: i32,  // 08
-}
-static_assert_size!(MeshesInfoC, 12);
-
-pub(super) fn read_meshes(
+pub fn read_meshes(
     read: &mut CountingReader<impl Read>,
     end_offset: u32,
 ) -> Result<(Vec<MeshNg>, i32)> {
-    debug!(
-        "Reading mesh info (pm, {}) at {}",
-        MeshesInfoC::SIZE,
-        read.offset
-    );
-    let info: MeshesInfoC = read.read_struct()?;
-    trace!("{:#?}", info);
-
-    assert_that!("mesh count", info.count < info.array_size, read.prev + 4)?;
-    assert_that!(
-        "mesh index max",
-        info.index_max == info.count,
-        read.prev + 8
-    )?;
+    let (mesh_array_size, mesh_count) = read_meshes_info_sequential(read)?;
 
     let mut prev_offset = read.offset;
-    let meshes = (0..info.count)
+    let meshes = (0..mesh_count)
         .map(|mesh_index| {
             let wrapped_mesh = read_mesh_info(read, mesh_index)?;
             let mesh_offset = read.read_u32()?;
@@ -47,7 +27,7 @@ pub(super) fn read_meshes(
         })
         .collect::<Result<Vec<_>>>()?;
 
-    read_mesh_infos_zero(read, info.count, info.array_size)?;
+    read_mesh_infos_zero(read, mesh_count, mesh_array_size)?;
 
     let meshes = meshes
         .into_iter()
@@ -58,32 +38,21 @@ pub(super) fn read_meshes(
         })
         .collect::<Result<Vec<_>>>()?;
 
-    Ok((meshes, info.array_size))
+    Ok((meshes, mesh_array_size))
 }
 
-pub(super) fn write_meshes(
+pub fn write_meshes(
     write: &mut CountingWriter<impl Write>,
     meshes: &[MeshNg],
     offsets: &[u32],
     array_size: i32,
 ) -> Result<()> {
-    debug!(
-        "Writing mesh info (pm, {}) at {}",
-        MeshesInfoC::SIZE,
-        write.offset
-    );
     let count = assert_len!(i32, meshes.len(), "GameZ meshes")?;
-    let info = MeshesInfoC {
-        array_size,
-        count,
-        index_max: count,
-    };
-    trace!("{:#?}", info);
-    write.write_struct(&info)?;
+    write_meshes_info_sequential(write, array_size, count)?;
 
-    for (mesh_index, (mesh, offset)) in meshes.iter().zip(offsets.iter()).enumerate() {
+    for (mesh_index, (mesh, offset)) in meshes.iter().zip(offsets.iter().copied()).enumerate() {
         write_mesh_info(write, mesh, mesh_index)?;
-        write.write_u32(*offset)?;
+        write.write_u32(offset)?;
     }
 
     write_mesh_infos_zero(write, count, array_size)?;
@@ -100,7 +69,7 @@ const U32_SIZE: u32 = std::mem::size_of::<u32>() as _;
 pub fn size_meshes(offset: u32, array_size: i32, meshes: &[MeshNg]) -> (u32, Vec<u32>) {
     // Cast safety: truncation simply leads to incorrect size (TODO?)
     let array_size = array_size as u32;
-    let mut offset = offset + MeshesInfoC::SIZE + (MESH_C_SIZE + U32_SIZE) * array_size;
+    let mut offset = offset + MESHES_INFO_C_SIZE + (MESH_C_SIZE + U32_SIZE) * array_size;
     let mesh_offsets = meshes
         .iter()
         .map(|mesh| {
