f67e3f2e6fea40aea4f18dac6fffa1651f95c7a0
Toby Fleming <sourcecode@tobywf.com>
2022-12-17 13:16:41 -0800

    Add debug logging to motions code

--- crates/mech3ax-motion/src/motion.rs
+++ crates/mech3ax-motion/src/motion.rs
@@ -1,37 +1,58 @@
+use log::{debug, trace};
 use mech3ax_api_types::motion::{Motion, MotionFrame, MotionPart};
-use mech3ax_api_types::static_assert_size;
+use mech3ax_api_types::{static_assert_size, ReprSize as _};
 use mech3ax_common::io_ext::{CountingReader, CountingWriter};
 use mech3ax_common::{assert_len, assert_that, assert_with_msg, Result};
 use std::io::{Read, Write};
 
 const VERSION: u32 = 4;
-const FLAGS: u32 = 8 | 4;
+const FLAG_TRANSLATION: u32 = 1 << 3;
+const FLAG_ROTATION: u32 = 1 << 2;
+#[allow(unused)]
+const FLAG_SCALING: u32 = 1 << 1; // never in motion.zbd
+const FLAGS: u32 = FLAG_TRANSLATION | FLAG_ROTATION;
 
+#[derive(Debug)]
 #[repr(C)]
-struct Header {
-    version: u32,
-    loop_time: f32,
-    frame_count: u32,
-    part_count: u32,
-    minus_one: f32,
-    plus_one: f32,
+struct MotionHeader {
+    version: u32,     // 00
+    loop_time: f32,   // 04
+    frame_count: u32, // 08
+    part_count: u32,  // 12
+    unk16: f32,       // 16
+    unk20: f32,       // 20
 }
-static_assert_size!(Header, 24);
+static_assert_size!(MotionHeader, 24);
 
 pub fn read_motion(read: &mut CountingReader<impl Read>) -> Result<Motion> {
-    let header: Header = read.read_struct()?;
-    assert_that!("version", header.version == VERSION, read.prev)?;
-    assert_that!("loop time", header.loop_time > 0.0, read.prev + 4)?;
-    assert_that!("field 16", header.minus_one == -1.0, read.prev + 16)?;
-    assert_that!("field 20", header.plus_one == 1.0, read.prev + 20)?;
+    debug!(
+        "Reading motion header ({}) at {}",
+        MotionHeader::SIZE,
+        read.offset
+    );
+    let header: MotionHeader = read.read_struct()?;
+    trace!("{:#?}", header);
+
+    assert_that!("motion version", header.version == VERSION, read.prev)?;
+    assert_that!("motion loop time", header.loop_time > 0.0, read.prev + 4)?;
+    assert_that!("motion field 16", header.unk16 == -1.0, read.prev + 16)?;
+    assert_that!("motion field 20", header.unk20 == 1.0, read.prev + 20)?;
 
     let frame_count = header.frame_count;
     let parts = (0..header.part_count)
-        .map(|_| {
+        .map(|index| {
+            debug!("Reading motion part {} at {}", index, read.offset);
             let part_name = read.read_string()?;
+            trace!("Motion part {}/`{}`", index, part_name);
+
             let flags = read.read_u32()?;
-            // 8 = translation, 4 = rotation, 2 = scaling (never in motion.zbd)
-            assert_that!("flag", flags == FLAGS, read.prev)?;
+            assert_that!("motion part flags", flags == FLAGS, read.prev)?;
+
+            trace!(
+                "Reading motion part {} translations at {}",
+                index,
+                read.offset
+            );
 
             let mut translations = (0..=frame_count)
                 .map(|_| read.read_struct())
@@ -39,13 +60,27 @@ pub fn read_motion(read: &mut CountingReader<impl Read>) -> Result<Motion> {
 
             // the first and last frames always match
             let first = translations.first().ok_or_else(|| {
-                assert_with_msg!("part `{}` didn't contain a single frame", part_name)
+                assert_with_msg!(
+                    "motion part {}/`{}` didn't contain a single frame",
+                    index,
+                    part_name
+                )
             })?;
             let last = translations.last().ok_or_else(|| {
-                assert_with_msg!("part `{}` didn't contain a single frame", part_name)
+                assert_with_msg!(
+                    "motion part {}/`{}` didn't contain a single frame",
+                    index,
+                    part_name
+                )
             })?;
-            assert_that!("part translation first/last", first == last, read.offset)?;
+            assert_that!(
+                "motion part translation first/last",
+                first == last,
+                read.offset
+            )?;
             translations.pop();
+
+            trace!("Reading motion part {} rotations at {}", index, read.offset);
 
             let mut rotations = (0..=frame_count)
                 .map(|_| read.read_struct())
@@ -53,12 +88,24 @@ pub fn read_motion(read: &mut CountingReader<impl Read>) -> Result<Motion> {
 
             // the first and last frames always match
             let first = rotations.first().ok_or_else(|| {
-                assert_with_msg!("part `{}` didn't contain a single frame", part_name)
+                assert_with_msg!(
+                    "motion part {}/`{}` didn't contain a single frame",
+                    index,
+                    part_name
+                )
             })?;
             let last = rotations.last().ok_or_else(|| {
-                assert_with_msg!("part `{}` didn't contain a single frame", part_name)
+                assert_with_msg!(
+                    "motion part {}/`{}` didn't contain a single frame",
+                    index,
+                    part_name
+                )
             })?;
-            assert_that!("part rotation first/last", first == last, read.offset)?;
+            assert_that!(
+                "motion part rotation first/last",
+                first == last,
+                read.offset
+            )?;
             rotations.pop();
 
             let frames = translations
@@ -76,6 +123,7 @@ pub fn read_motion(read: &mut CountingReader<impl Read>) -> Result<Motion> {
         })
         .collect::<Result<Vec<_>>>()?;
 
+    debug!("Finished reading motions at {}", read.offset);
     read.assert_end()?;
 
     Ok(Motion {
@@ -86,36 +134,69 @@ pub fn read_motion(read: &mut CountingReader<impl Read>) -> Result<Motion> {
 }
 
 pub fn write_motion(write: &mut CountingWriter<impl Write>, motion: &Motion) -> Result<()> {
+    debug!(
+        "Writing motion header ({}) at {}",
+        MotionHeader::SIZE,
+        write.offset
+    );
     let part_count = assert_len!(u32, motion.parts.len(), "motion parts")?;
-    let header = Header {
+    let header = MotionHeader {
         version: VERSION,
         loop_time: motion.loop_time,
         frame_count: motion.frame_count,
         part_count,
-        minus_one: -1.0,
-        plus_one: 1.0,
+        unk16: -1.0,
+        unk20: 1.0,
     };
+    trace!("{:#?}", header);
     write.write_struct(&header)?;
 
-    for part in &motion.parts {
+    for (index, part) in motion.parts.iter().enumerate() {
+        debug!(
+            "Writing motion part {}/`{}` at {}",
+            index, part.name, write.offset
+        );
         write.write_string(&part.name)?;
         write.write_u32(FLAGS)?;
 
-        let first = part.frames.first();
+        let frame_count = assert_len!(u32, part.frames.len(), "motion part frames")?;
+        assert_that!(
+            "motion part frame count",
+            frame_count == motion.frame_count,
+            write.offset
+        )?;
+
+        let first = part.frames.first().ok_or_else(|| {
+            assert_with_msg!(
+                "motion part {}/`{}` didn't contain a single frame",
+                index,
+                part.name
+            )
+        })?;
+
+        trace!(
+            "Writing motion part {} translations at {}",
+            index,
+            write.offset
+        );
 
         for frame in &part.frames {
             write.write_struct(&frame.translation)?;
         }
-        if let Some(first) = first {
-            write.write_struct(&first.translation)?;
-        }
+        write.write_struct(&first.translation)?;
+
+        trace!(
+            "Writing motion part {} rotations at {}",
+            index,
+            write.offset
+        );
 
         for frame in &part.frames {
             write.write_struct(&frame.rotation)?;
         }
-        if let Some(first) = first {
-            write.write_struct(&first.rotation)?;
-        }
+        write.write_struct(&first.rotation)?;
     }
+
+    debug!("Finished writing motions at {}", write.offset);
     Ok(())
 }
