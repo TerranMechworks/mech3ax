b67a926411546cd9124fcf2980bda805d7ac5a58
Toby Fleming <sourcecode@tobywf.com>
2024-09-04 17:55:02 -0700

    Avoid proc-macro for bitflags

--- crates/types/src/bitflags/mod.rs
+++ crates/types/src/bitflags/mod.rs
@@ -1,41 +1,14 @@
 mod display_set;
+mod flags;
+
 use bytemuck::{AnyBitPattern, NoUninit, Zeroable};
-use display_set::DisplaySet;
-pub use mech3ax_types_proc_macro::bitflags_derive;
+pub use flags::{
+    format_flags_u16, format_flags_u32, format_flags_u8, gather_flags_u16, gather_flags_u32,
+    gather_flags_u8,
+};
 use std::fmt;
 use std::marker::PhantomData;
 use std::ops::{BitAnd, BitOr, BitOrAssign, Not};
-
-const FLAGS_RAW: &[&str; 32] = &[
-    "1 << 0", "1 << 1", "1 << 2", "1 << 3", "1 << 4", "1 << 5", "1 << 6", "1 << 7", "1 << 8",
-    "1 << 9", "1 << 10", "1 << 11", "1 << 12", "1 << 13", "1 << 14", "1 << 15", "1 << 16",
-    "1 << 17", "1 << 18", "1 << 19", "1 << 20", "1 << 21", "1 << 22", "1 << 23", "1 << 24",
-    "1 << 25", "1 << 26", "1 << 27", "1 << 28", "1 << 29", "1 << 30", "1 << 31",
-];
-
-macro_rules! fmt_flags {
-    ($name:ident, $ty:ty, $bits:literal) => {
-        #[inline]
-        pub fn $name(
-            v: $ty,
-            f: &mut fmt::Formatter<'_>,
-            flags: &'static [Option<&'static str>; $bits],
-        ) -> fmt::Result {
-            let mut set = DisplaySet::new(f);
-            for index in 0..$bits {
-                if v & (1 << index) != 0 {
-                    let flag = flags[index].unwrap_or(FLAGS_RAW[index]);
-                    set.entry(&flag);
-                }
-            }
-            set.finish()
-        }
-    };
-}
-
-fmt_flags!(format_flags_u8, u8, 8);
-fmt_flags!(format_flags_u16, u16, 16);
-fmt_flags!(format_flags_u32, u32, 32);
 
 pub trait BitflagsRepr
 where
@@ -99,14 +72,15 @@ macro_rules! bitflags {
         #[repr(transparent)]
         $vis struct $name($ty);
 
-        $crate::bitflags::bitflags_derive! {
-            $name $ty {
-                $($flag => $val,)+
-            }
-        }
-
         impl $name {
+            const VARIANTS: &'static [(usize, &'static str)] = &[
+                $(($val, stringify!($flag)),)+
+            ];
+            bitflags!(@flags $ty);
+
+            const VALID: $ty = 0 $(| (1 << $val))+;
             const INVALID: $ty = !Self::VALID;
+
             $(pub const $flag: Self = Self(1 << $val);)+
 
             #[inline]
@@ -210,15 +184,6 @@ macro_rules! bitflags {
             }
         }
     };
-    (@bits u8) => {
-        8
-    };
-    (@bits u16) => {
-        16
-    };
-    (@bits u32) => {
-        32
-    };
     (@fmt u8) => {
         $crate::bitflags::format_flags_u8
     };
@@ -227,6 +192,15 @@ macro_rules! bitflags {
     };
     (@fmt u32) => {
         $crate::bitflags::format_flags_u32
+    };
+    (@flags u8) => {
+        const FLAGS: &'static [::core::option::Option<&'static str>; 8] = &$crate::bitflags::gather_flags_u8(Self::VARIANTS);
+    };
+    (@flags u16) => {
+        const FLAGS: &'static [::core::option::Option<&'static str>; 16] = &$crate::bitflags::gather_flags_u16(Self::VARIANTS);
+    };
+    (@flags u32) => {
+        const FLAGS: &'static [::core::option::Option<&'static str>; 32] = &$crate::bitflags::gather_flags_u32(Self::VARIANTS);
     };
 }
 
